eddf8ae025bb98be9308524393460b422210f4d8
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-12-18 16:00:08 +0700

    add script sql

---
+++ registry.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.registry (name,url,access_key,access_secret,insecure,creation_time,update_time,credential_type,"type",description,health) VALUES
+	 ('ghcr.io','https://ghcr.io','','',false,'2023-11-22 06:15:44.701','2023-11-22 07:18:06.872','','github-ghcr','','healthy'),
+	 ('registry.k8s.io','https://registry.k8s.io','','',false,'2023-11-23 04:14:05.311','2023-11-23 04:35:02.306','','docker-registry','','healthy'),
+	 ('gcr','https://gcr.io','','',true,'2023-11-23 06:22:50.533','2023-11-23 06:22:50.533','','docker-registry','','healthy'),
+	 ('k8s.gcr.io','https://k8s.gcr.io','','',false,'2023-11-23 07:04:19.474','2023-11-23 07:04:19.474','','docker-registry','','healthy'),
+	 ('hub.docker.com','https://hub.docker.com','','',false,'2023-11-22 08:37:03.151','2023-12-04 03:30:09.538','','docker-hub','','healthy'),
+	 ('quay.io','https://quay.io','','',false,'2023-11-22 06:05:14.623','2023-12-04 16:55:13.764','','quay','','healthy'),
+	 ('docker_io','https://docker.io','','',false,'2023-11-29 06:09:45.411','2023-12-11 01:52:47.945','','docker-registry','','healthy'),
+	 ('asia.gcr.io','https://asia.gcr.io','','',false,'2023-12-12 04:22:31.523','2023-12-12 04:22:31.523','','docker-registry','','healthy');
